db9de0be62cc5bcfa29c42062b46069386e702a2
pelayocurso <pelayocurso@gmail.com>
2017-09-01 18:44:09 +0200

    Añadidos componentes reutilizables, estructura de maestro-detalle de eventos

---
+++ src/EventoComponent/Evento.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="detail" v-if="evento">
+    <form>
+      <div class="form-group">
+        <label for="fecha-evento"></label>
+        <input type="date" class="form-control" id="fecha-evento" v-model="evento.Fecha" />
+      </div>
+      <div class="form-group">
+        <label for="descripcion-evento"></label>
+        <input type="text" class="form-control" id="descripcion-evento" v-model="evento.Descripcion" />
+      </div>
+      <div class="form-group">
+        <label for="tipo-evento"></label>
+        <input type="text" class="form-control" id="tipo-evento" v-model="evento.Tipo" />
+      </div>
+      <div class="form-group">
+        <label for="origen-evento"></label>
+        <input type="text" class="form-control" id="origen-evento" v-model="evento.Origen" />
+      </div>
+      <div class="form-group">
+        <label for="destino-evento"></label>
+        <input type="text" class="form-control" id="destino-evento" v-model="evento.Destino" />
+      </div>
+      <div class="form-group">
+        <label for="prioridad-evento"></label>
+        <input type="text" class="form-control" id="prioridad-evento" v-model="evento.Prioridad" />
+      </div>
+
+      <div class="group-btn">
+        <button v-if="evento.Id" class="btn btn-success" @click="handleModificarEvento">Modificar</button>
+        <button v-else class="btn btn-success" @click="handleCrearEvento">Crear</button>
+        <button class="btn btn-secondary" @click="handleCancelar">Cancelar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'evento',
+    props: ['api_host'],
+
+    data() {
+      evento: null,
+      host: this.api_host
+    },
+
+    created() {
+      let _this = this;
+      Vue.$on('show-form', (evento) => {
+          _this.evento = evento ? evento : { Fecha: '', Descripcion: '', Tipo: '', Origen: '', Destino: '', Prioridad: ''};
+      });
+
+      Vue.$on('close-form', () => {
+        _this.evento = null
+      });
+    },
+
+    methods: {
+      /* SERVER REQUESTS */
+      updateEvento() {
+
+      },
+
+      createEvento() {
+
+      },
+
+      /* HANDLE SELF EVENTS */
+      handleModificarEvento() {
+
+      },
+
+      handleCrearEvento() {
+
+      },
+
+      handleCancelar() {
+
+      }
+    }
+  }
+</script>
---
+++ src/EventoComponent/EventoMaster.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="master">
+    <div class="detail">
+      <button class="btn btn-primary" @click="this.handleCrearNuevoEventoClick">Crear nuevo evento</button>
+      <ul>
+        <li v-for="evento in eventos" >
+          {{ evento.Nombre }}
+          <button class="btn btn-default" @click="this.handleEditarClick(evento)">Editar</button>
+          <button class="btn btn-danger"@click="this.handleBorrarClick(evento)">Borrar</button>
+        </li>
+      </ul>
+    </div>
+
+    <evento @addEvento="" @modifyEvento=""></evento>
+  </div>
+</template>
+
+<script>
+  import Evento from './Evento.vue';
+  import axios from 'axios';
+
+  export default {
+    name: 'evento-master',
+    data() {
+      return {
+        eventos: [],
+        host: 'http://localhost:8080/api/Eventos',
+      };
+    },
+
+    created() {
+      this.getAllEventos();
+    },
+
+    methods: {
+      /* SERVER REQUESTS */
+      getAllEventos() {
+        axios.get(this.host).then((response) => {
+
+        }).catch((error) => {
+
+        });
+      },
+
+      deleteEvento(evento) {
+        axios.delete(this.host + '/' + evento.Id).then((response) => {
+
+        }).catch((error) => {
+
+        });
+      },
+
+      /* HANDLE SELF EVENTS */
+      handleCrearNuevoEventoClick() {
+
+      },
+
+      handleEditarClick(evento) {
+
+      },
+
+      handleBorrarClick(evento) {
+
+      },
+
+      /* HANDLE CHILDREN EVENTS */
+      onAddEvento(evento) {
+
+      },
+
+      onModifyEvento(evento) {
+
+      },
+
+    },
+  }
+</script>
+
+<style>
+</style>
