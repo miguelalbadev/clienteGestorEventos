a655104939be403463215ca8d879eeb6196fa220
unknown <miguelalbadev@gmail.com>
2017-09-07 15:18:44 +0200

    Añadido título al html

--- src/TabComponents/TabMaster.vue
+++ src/TabComponents/TabMaster.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <ul class="nav nav-tabs">
+  <h2>Sistema Gestor de Eventos</h2>
+  <ul class="nav nav-tabs">
       <li v-for="tab in tabs" class="nav-item">
         <a class="nav-link" :class="{'active' : tab.activated}" data-toggle="tab" :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
       </li>
@@ -51,3 +52,39 @@
     }
   };
 </script>
+<style>
+h2{
+position:relative;
+right:-2px;
+padding:8px 63px 6px 17px; 
+width:100%;
+margin:0;
+font-size:32px;
+background:#4F4F4F; /* Background color */
+color:#f2f2f2; /* Color of the widget's title */
+text-align:center;
+text-indent:18px;
+}
+h2:before{
+position:absolute;
+content:"";
+top:36px;
+right:0px;
+width: 0px;
+height: 0px;
+border-bottom:12px outset transparent;
+border-left:12px solid #000000; 
+}
+h2:after{
+position:absolute;
+content:"";
+top:-6px;
+left:-5px;
+width: 0px;
+height: 0px;
+border-bottom:24px outset transparent;
+border-top:24px outset transparent;
+border-left:24px solid #ffffff; /* Arrow background color on the left */
+}
+
+</style>
